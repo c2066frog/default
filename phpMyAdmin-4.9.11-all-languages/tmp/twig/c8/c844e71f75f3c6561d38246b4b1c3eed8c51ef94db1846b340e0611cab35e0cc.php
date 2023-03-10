a1bcec9d68d7375c7e5ead07334e5daa978d32e0
git <git@dev.local>
2023-03-10 15:17:17 +0800

    git-push

---
+++ phpMyAdmin-4.9.11-all-languages/tmp/twig/c8/c844e71f75f3c6561d38246b4b1c3eed8c51ef94db1846b340e0611cab35e0cc.php
@@ -0,0 +1,55 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* login/footer.twig */
+class __TwigTemplate_c564218106ca02dab39e9332f9eda7526b8bbea96ce892e3176790e9d1ae8af2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "login/footer.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "login/footer.twig", "D:\\Proj\\proj-dev\\default\\phpMyAdmin-4.9.11-all-languages\\templates\\login\\footer.twig");
+    }
+}
